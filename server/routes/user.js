32f5950a69549792d8b5a653d1d4ec434c17cc46
Charu Lata <charu.lata@ibm.com>
2022-06-20 22:16:44 +0530

    first commit

---
+++ server/routes/user.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+//create,find,update,delete
+
+router.get('/',userController.view);
+router.post('/',userController.find);
+router.get('/adduser',userController.form);
+router.post('/adduser',userController.create);
+router.get('/edituser/:id',userController.edit);
+router.post('/edituser/:id',userController.update);
+router.get('/viewuser/:id',userController.viewall);
+router.get('/:id',userController.delete);
+
+// get  : when just have to show what we have
+// post : when we need to get data from user 
+module.exports = router;
